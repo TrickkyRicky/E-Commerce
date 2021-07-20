d7c08ecd12bc93397c81613a81c92f8f7e8c9120
trickkyricky <rickygyoungjr@gmail.com>
2021-07-20 18:26:20 -0500

    added products component and line to seperate it from Hero

--- FrontEnd/src/pages/home/home.jsx
+++ FrontEnd/src/pages/home/home.jsx
@@ -1,11 +1,15 @@
+// page with hero image slider and recently added products
 import React from "react";
 import classes from "./Home.module.scss";
 import Hero from "../../components/UI/heroSlider/Hero";
+import NewProducts from "../../components/content/newProducts/NewProducts";
 
 const Home = () => {
   return (
     <div className={classes.container}>
       <Hero />
+      <div className={classes.line}></div>
+      <NewProducts />
     </div>
   );
 };
