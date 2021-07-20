1ab4009b9542b4438552a6b4c6a4c8076fa2b582
trickkyricky <rickygyoungjr@gmail.com>
2021-07-20 18:21:32 -0500

    Completion of footer along with icons and apple/android badges

--- FrontEnd/src/components/layout/footer/footer.jsx
+++ FrontEnd/src/components/layout/footer/footer.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import classes from "./Footer.module.scss";
+import {
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+import { FaSnapchatGhost, FaPinterestP } from "react-icons/fa";
+
+import asb from "../../../assets/appleBadge.svg";
+import gpb from "../../../assets/googleBadge.svg";
 
 const Footer = (props) => {
   return (
     <div className={classes.container}>
-      <div>
-        <p>Social media icons here</p>
+      <div className={classes.icons}>
+        <a href="/">
+          <AiOutlineTwitter size="27" color="white" />
+        </a>
+        <a href="/">
+          <AiOutlineInstagram size="27" color="white" />
+        </a>
+        <a href="/">
+          <AiOutlineFacebook size="27" color="white" />
+        </a>
+        <a href="/">
+          <FaSnapchatGhost size="25" color="white" />
+        </a>
+        <a href="/">
+          <FaPinterestP size="25" color="white" />
+        </a>
       </div>
-      <div>
-        <p>Android and Apple Play Store Here</p>
+      <div className={classes.download}>
+        <a href="/">
+          <img src={asb} alt="apple store download icon" />
+        </a>
+        <a href="/">
+          <img src={gpb} alt="google play download icon" />
+        </a>
       </div>
-      <div>
-        <p>Created by Richard Young and copyright</p>
+      <div className={classes.terms}>
+        <p>
+          Terms &#38; Conditions | Integration | &copy; 2021 By Richard Young.
+          All rights reserved.
+        </p>
       </div>
     </div>
   );
