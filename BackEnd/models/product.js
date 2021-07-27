7941034cd6e7a875937735bb7fe190327c01bd70
trickkyricky <rickygyoungjr@gmail.com>
2021-07-27 17:02:37 -0500

    product and user models finalized

---
+++ BackEnd/models/product.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      xsmall: { type: Number, required: true },
+      small: { type: Number, required: true },
+      medium: { type: Number, required: true },
+      large: { type: Number, required: true },
+      xlarge: { type: Number, required: true },
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    sale: {
+      type: Number,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
