9510e6c091e47e04b84d0a7ccb1479fc537a8894
trickkyricky <rickygyoungjr@gmail.com>
2021-07-27 17:13:55 -0500

    added in react router and token for local storage to check if route should be renedered yet

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-
+import { useSelector, useDispatch } from "react-redux";
 import Auth from "./pages/auth/Auth.jsx";
 import Home from "./pages/home/Home.jsx";
 import Shop from "./pages/shop/Shop.jsx";
@@ -8,9 +9,25 @@ import Products from "./pages/products/Products.jsx";
 import ProductDetail from "./pages/productDetail/ProductDetail.jsx";
 import NotFound from "./pages/notFound/NotFound.jsx";
 
+import { authActions } from "./store/auth/auth-slice.js";
 import "./App.scss";
 
-function App() {
+const App = () => {
+  // Remeber to check here with useSelector to check if still authenticated
+  // also if authenticated then dont show the auth paths conditionally
+  const isAuth = useSelector((state) => state.auth.isAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const jwtToken = localStorage.getItem("token");
+    if (!jwtToken) {
+      return;
+    }
+    const user = localStorage.getItem("userId");
+    dispatch(authActions.successLogin({ jwtToken: jwtToken, userId: user }));
+    dispatch(authActions.setAuth(true));
+  }, [dispatch]);
+
   return (
     <Layout>
       <Switch>
@@ -23,12 +40,23 @@ function App() {
         <Route path="/shop">
           <Shop />
         </Route>
-        <Route path="/auth">
+        <Route path="/auth" exact>
           <Auth />
         </Route>
-        <Route path="/products">
-          <Products />
+        <Route path="/auth/signup">
+          <Auth />
         </Route>
+        <Route path="/auth/reset">
+          <Auth />
+        </Route>
+        <Route path="/auth/newpass/:token">
+          <Auth />
+        </Route>
+        {isAuth && (
+          <Route path="/myProducts">
+            <Products />
+          </Route>
+        )}
         <Route path="/productDetail/:productId">
           <ProductDetail />
         </Route>
@@ -38,6 +66,6 @@ function App() {
       </Switch>
     </Layout>
   );
-}
+};
 
 export default App;
