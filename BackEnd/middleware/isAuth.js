18335a9fee63b68c31b68761b7765575c4840369
trickkyricky <rickygyoungjr@gmail.com>
2021-07-27 17:02:22 -0500

    isAuth middleware checks and verifies user is authenticated on a guarded route

---
+++ BackEnd/middleware/isAuth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  // sets header for finding attached token from the frontend
+  // console.log(req.headers);
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    const error = new Error("Not authenticated pt 1");
+    error.statusCode = 401;
+    throw error;
+  }
+  //   split to get the headers token value
+  const token = authHeader.split(" ")[1];
+  console.log("TOKEN IN MIDDLEWARE:", token);
+  let decodedToken;
+
+  // decode the token with the secret key pass
+  try {
+    decodedToken = jwt.verify(token, process.env.JPASS);
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+
+  if (!decodedToken) {
+    const error = new Error("Not authenticated pt 2");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
