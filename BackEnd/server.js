2861be2f0ed23883b1314b37ee770d1e92c46e57
trickkyricky <rickygyoungjr@gmail.com>
2021-07-27 17:05:22 -0500

    server setup

---
+++ BackEnd/server.js
@@ -0,0 +1,105 @@
+// used to forge a direct path to the images folder
+const path = require("path");
+// framework for node js
+const express = require("express");
+// used to parse buffers from incoming body req via post,put,etc.
+const bodyParser = require("body-parser");
+// used to make querying easier to mongodb
+const mongoose = require("mongoose");
+// used to parse incoming body req jpeg, jpg, png images for website
+const multer = require("multer");
+
+const server = express();
+
+// access to env variables
+require("dotenv").config();
+
+/* --------- Routes ---------- */
+
+const authRoute = require("./routes/auth.js");
+const adminRoute = require("./routes/admin.js");
+const shopRoute = require("./routes/shop.js");
+
+/* --------- Image handling ---------- */
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file);
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  console.log("FILE FILTER CHECKER HERE:::::!:!!!! ", file);
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+/* --------- middleware ---------- */
+
+// might change back to bodyParser.json() <---is deprecated
+server.use(express.json());
+
+// uses the defined filter and storage abouve to parse image into place
+server.use(
+  multer({
+    limits: { fieldSize: 25 * 1024 * 1024 },
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("image")
+);
+// serves images statically front end
+server.use("/images", express.static(path.join(__dirname, "images")));
+
+// allows the use of REST from other clients on other Ports via the following request
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// route middleware with built in validation
+server.use("/auth", authRoute);
+server.use("/admin", adminRoute);
+server.use("/shop", shopRoute);
+
+// error redirection middleware
+server.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.DB_URI, {
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    pass: process.env.DB_PASS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    server.listen(process.env.PORT || 8080);
+  })
+  .then(() => {
+    console.log("MONGO CONNECTED");
+  })
+  .catch(() => {
+    console.log("WE DIDNT CONNECT");
+  });
