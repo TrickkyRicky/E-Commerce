cb0f3675e92ee4937c994e3dba2785456c31e816
trickkyricky <rickygyoungjr@gmail.com>
2021-10-26 09:52:36 -0500

    test file oathing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import Layout from "./components/layout/Layout.jsx";
+import Layout from "components/layout/Layout.jsx";
 import Home from "./pages/home/Home.jsx";
 import Shop from "./pages/shop/Shop.jsx";
 import Auth from "./pages/auth/Auth.jsx";
