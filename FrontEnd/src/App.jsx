edb2af8e4b2f62acb61bd067c8be95b81b750de4
trickkyricky <rickygyoungjr@gmail.com>
2021-10-26 10:13:34 -0500

    caps the file path

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import Layout from "./components/Layout/Layout.jsx";
+import Layout from "./Components/Layout/Layout.jsx";
 import Home from "./Pages/Home/Home.jsx";
 import Shop from "./Pages/Shop/Shop.jsx";
 import Auth from "./Pages/Auth/Auth.jsx";
