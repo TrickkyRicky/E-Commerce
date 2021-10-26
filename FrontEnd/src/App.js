47aefda090cc69edace9eb372388bd66df6366bc
trickkyricky <rickygyoungjr@gmail.com>
2021-10-25 23:17:20 -0500

    test

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import Home from "./pages/home/Home.jsx";
+import Home from "./pages/Home/Home.jsx";
 import Shop from "./pages/shop/Shop.jsx";
 import Auth from "./pages/auth/Auth.jsx";
 import Layout from "./components/layout/Layout.jsx";
